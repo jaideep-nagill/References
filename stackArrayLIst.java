add9e66cdba56a29f3788154d301b5ad0f456499
Jaideepagill <67574627+Jaideepagill@users.noreply.github.com>
2022-03-13 00:15:09 +0530

    Create stackArrayLIst.java

---
+++ stackArrayLIst.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+class Stack {
+  private ArrayList<Integer> arr = new ArrayList<>();
+
+  public void push(int x){
+    arr.add(x);
+  }
+
+  public int pop(){
+    if ( arr.size() == 0)
+      return -1;
+    int last = arr.get(arr.size() - 1);
+    arr.remove(arr.size() - 1);
+    return last;
+  }
+
+  public boolean isEmpty(){
+    if ( arr.size() == 0)
+      return true;
+    return false;
+  }
+
+  public int peek(){
+    return arr.get(arr.size() -1);
+  }
+}
+
+public class Main{
+  public static void main(String [] args){
+    Stack s = new Stack();
+    for (int i = 1; i <=10; i++)
+      s.push(i);
+    s.push(11);
+    System.out.println(s.pop());
+    System.out.println(s.pop());
+  }
+}
