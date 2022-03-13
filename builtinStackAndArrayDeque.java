d4954f9aada095cea80e35cee57b0c3d0d128bb0
Jaideepagill <67574627+Jaideepagill@users.noreply.github.com>
2022-03-13 14:20:50 +0530

    Create builtinStackAndArrayDeque.java

---
+++ builtinStackAndArrayDeque.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+import java.util.ArrayDeque;
+
+public class Main{
+  public static void main(String [] args){
+    Stack<Integer> st = new Stack<Integer>();
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    st.push(6);
+    System.out.println(st.pop());
+    System.out.println(st.peek());
+    System.out.println(st.isEmpty());
+    System.out.println(st);
+
+    ArrayDeque<Integer> ad = new ArrayDeque<Integer>();
+    ad.push(1);
+    ad.push(2);
+    ad.push(3);
+    ad.push(4);
+    ad.push(5);
+    ad.push(6);
+    System.out.println(ad);
+    ad.pop();
+    System.out.println(ad.peek());
+  }
+}
+
