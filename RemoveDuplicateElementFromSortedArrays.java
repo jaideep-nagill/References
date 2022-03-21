5a8ad46d4827f63aaee0484d3de55365c130cb4a
Jaideepagill <67574627+Jaideepagill@users.noreply.github.com>
2022-03-21 21:50:54 +0530

    Create RemoveDuplicateElementFromSortedArrays.java

---
+++ RemoveDuplicateElementFromSortedArrays.java
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class RemoveDuplicateElementFromSortedArrays {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T>0)
+        {
+            int N = sc.nextInt();
+            int a[] = new int[N];
+            for(int i=0; i<N; i++)
+                a[i] = sc.nextInt();
+            
+            Solution g = new Solution();
+            int n = g.remove_duplicate(a,N);
+            
+            for(int i=0; i<n; i++)
+                System.out.print(a[i]+" ");
+            System.out.println();
+            T--;
+        }
+        sc.close();
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int remove_duplicate(int A[],int N){
+        // code here
+        int res = 1;
+        for ( int i = 1; i < N; i++)
+          if ( A[res-1] != A[i])
+            A[res++] = A[i];
+        return res;
+    }
+}
