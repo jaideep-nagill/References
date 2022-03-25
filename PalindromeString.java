e0d380df8fbdeee04246c171243440a91d8c6ba0
Jaideepagill <67574627+Jaideepagill@users.noreply.github.com>
2022-03-25 23:34:20 +0530

    Update PalindromeString.java

--- PalindromeString.java
+++ PalindromeString.java
@@ -31,3 +31,20 @@ class Solution {
         return S.equals(res) ? 1 : 0;
     }
 };
+//***********************************************************************************
+
+class Main{
+  public static void main(String [] args){
+    String str = "abcddcb";
+    boolean flag = true;
+    for(int i = 0,  j = str.length()-1; i < j; i++, j--){
+      System.out.println(i + " " + j);
+      if ( str.charAt(i) != str.charAt(j)){
+        flag = false;
+        break;
+      }
+    }
+    System.out.println(flag ? "It is a Palindrome String" : "It is not a Palindrome String");
+  }
+}
+
